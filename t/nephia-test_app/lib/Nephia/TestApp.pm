94baad4d1a9187cb3d03a7685b0655e54b662ed1
Satoshi Azuma <ytnobody@gmail.com>
2013-07-06 07:40:50 +0000

    Import of YTURTLE/Nephia-0.26 from CPAN.
    gitpan-cpan-distribution: Nephia
    gitpan-cpan-version:      0.26
    gitpan-cpan-path:         YTURTLE/Nephia-0.26.tar.gz
    gitpan-cpan-author:       YTURTLE
    gitpan-cpan-maturity:     released

--- t/nephia-test_app/lib/Nephia/TestApp.pm
+++ t/nephia-test_app/lib/Nephia/TestApp.pm
@@ -4,7 +4,7 @@ use warnings;
 use Nephia plugins => qw/Bark/;
 use utf8;
 
-our $VERSION = 0.25;
+our $VERSION = 0.26;
 
 my $item = 'ひのきのぼう';
 
@@ -78,6 +78,12 @@ post '/item/{newitem:.+}' => sub {
     return {
         message => "$item　を　つかう",
     };
+};
+
+get '/with/{who:.+}' => sub {
+    my $who    = param()->{'who'};
+    my $action = param('action') || '踊った';
+    return { message => $who."と".$action };
 };
 
 path '/bark' => sub {
